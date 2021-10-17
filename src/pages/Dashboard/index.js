be1a9efcb074d6b1a2ef21d1b799c592ec2f0888
RobertaLima <80784099+Robertatlima@users.noreply.github.com>
2021-10-17 10:22:21 -0300

    Update index.js

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { useAuth } from "../../providers/Auth";
 import { useUser } from "../../providers/User";
-import { Link } from "react-router-dom";
+
 
 const Dashboard = () => {
   const history = useHistory();
@@ -28,22 +28,6 @@ const Dashboard = () => {
 
   return (
     <>
- feature/groups
-      <div>
-        <h1>ID do usuário para buscar na API: {user.id}</h1>
-        {auth ? (
-          <h2>State isLogged: true </h2>
-        ) : (
-          <h2>State isLogged: false </h2>
-        )}
-
-        <Link to="/groups">
-          <button>Groups</button>
-        </Link>
-        <Link to="/allGroups">
-          <button>Descobrir</button>
-        </Link>
-      </div>
 
       {auth ? (
         <div>
@@ -53,11 +37,16 @@ const Dashboard = () => {
           <Link to="/habits">
             <Button variant="contained">Habits</Button>
           </Link>
+  <Link to="/groups">
+            <button>Groups</button>
+          </Link>
+          <Link to="/allGroups">
+            <button>Descobrir</button>
+          </Link>
         </div>
       ) : (
         history.push("/login")
       )}
- developer
     </>
   );
 };
