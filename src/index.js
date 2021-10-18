05eb37e975616bc48d1b292c282d70796c93663a
Flávio Santos <flavioraph@live.com>
2021-10-18 11:07:23 -0300

    definição de reset e etilo global

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./App";
 
 import { BrowserRouter } from "react-router-dom";
 import Providers from "./providers";
+import GlobalStyle from "./styles/global";
+import GlobalStyleReset from "./styles/reset/reset";
 ReactDOM.render(
   <React.StrictMode>
-    <Providers>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Providers>
+   
+      <Providers>
+        <BrowserRouter>
+        <GlobalStyleReset/>
+        <GlobalStyle/>
+          <App />
+        </BrowserRouter>
+      </Providers>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,1 +1,10 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+background-color: var(--color-primary);
+font-family: var(--font-primary);
+height: 100vh;
+display: flex;
+align-items: center;
+justify-content: center;
+`
