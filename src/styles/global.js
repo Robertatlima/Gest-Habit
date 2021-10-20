26e359b36cc6fa755b412801de5b1c4dc1839e32
Rafael Kammer <rafa.br00@gmail.com>
2021-10-19 22:33:54 -0300

    Estilização da home finalizada

--- src/styles/global.js
+++ src/styles/global.js
@@ -3,33 +3,38 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
 :root{
     --color-primary: #8092ED;
-    --color-group: #54BF9F;
+    --color-group: #5472BF;
+    --color-secondary: #6AD980
+    --color-habits: #D9B24A;
+    --color-goals: #54BF9F;
     --color-ativities: #BF5454;
     --color-blacksmoky: #3D3F4B;
     --color-black: #333333;
     --color-gray50: #F5F5F5;
     --color-gray100: #7F7F7F;
     --font-primary: 'Roboto , sans-serif';
-
 }
 body{
-   background-color: #F5F5F5;
-
+   background-color: var(--color-gray50); 
    
 }
+h1, h2, h3, p{
+    font-family: "Rubik", sans-serif;;
+    font-style: normal;
+    color: var(--color-gray50);
+}
 h1{
-    font-family: 'Rubik', sans-serif;
     font-size: 50px;
 }
 h2{
-    font-family: 'Roboto , sans-serif';
     font-size: 32px;
 }
 h3{
-    font-family: 'Roboto , sans-serif';
     font-size: 18px;
 }
-
-
+p{
+    font-size: 16px;
+    font-weight: lighter;
+}
 `;
 export default GlobalStyle;
