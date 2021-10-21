2e205a7a487fa9104752e610f5f46391da8c4207
Rafael Kammer <rafa.br00@gmail.com>
2021-10-21 16:18:27 -0300

    Implementada edição de username

--- src/components/cardProfile/index.js
+++ src/components/cardProfile/index.js
@@ -3,11 +3,56 @@ import { useAuth } from "../../providers/Auth";
 import { useHistory, Link } from "react-router-dom";
 import imgProfile from "../../assets/img/profile.jpg";
 import * as C from "./styles.js";
+import Button from "../Button";
+import { useState } from "react";
+import axios from "axios";
+import { TextField } from "@mui/material";
 
 const CardProfile = () => {
   const history = useHistory();
   const { user, setUser } = useUser();
   const { auth } = useAuth();
+  const [edit, setEdit] = useState(true);
+
+  const handleEdit = () => {
+    setEdit(!edit);
+  };
+
+  const [name, setName] = useState(user.username);
+
+  const handleName = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleConfirm = () => {
+    const requestData = {
+      username: name,
+    };
+
+    const token = localStorage.getItem("token");
+
+    axios
+      .patch(
+        `https://kenzie-habits.herokuapp.com/users/${user.id}/`,
+        requestData,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      )
+      .then((response) => {
+        setUser({
+          id: response.data.id,
+          username: response.data.username,
+          email: response.data.email,
+        });
+      })
+      .then(() => {
+        setEdit(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <C.Container>
@@ -18,8 +63,36 @@ const CardProfile = () => {
               <img src={`${imgProfile}`} alt="" />
             </div>
             <div className="profile-right">
-              <h1>{user.username}</h1>
-              {/* <span>Editar perfil</span> */}
+              {edit ? (
+                <>
+                  <h1>{user.username}</h1>
+                  <Button
+                    className="editbutton"
+                    schema={edit}
+                    children={"Editar perfil"}
+                    onClick={handleEdit}
+                  />
+                </>
+              ) : (
+                <>
+                  <TextField
+                    defaultValue={user.username}
+                    onChange={handleName}
+                    variant="filled"
+                    id="title"
+                    label="Novo nome"
+                    margin="normal"
+                    size="medium"
+                    color="primary"
+                  />
+                  <Button
+                    className="editbutton"
+                    schema={edit}
+                    children={"Atualizar"}
+                    onClick={handleConfirm}
+                  />
+                </>
+              )}
             </div>
           </div>
           <hr />
