816b1a79889d445f9a79d1835f0bd6b8e50518cb
Rafael Kammer <rafa.br00@gmail.com>
2021-10-21 23:03:43 -0300

    Correção agora vai

--- src/pages/Groups/style.js
+++ src/pages/Groups/style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   width: 100%;
-  @media (max-width: 600px) {
+  @media (max-width: 700px) {
     flex-direction: column;
     align-items: center;
     justify-content: center;
@@ -14,30 +14,32 @@ export const ContainerPrincipal = styled.div`
   flex-wrap: wrap;
   flex-direction: column;
   margin-top: 15px;
+  width: 100%;
 `;
 
 export const ButtonsContainer = styled.div`
-  @media (min-width: 700px){
+  @media (min-width: 1100px) {
     display: flex;
-    margin-left: 30px;
-    width:820px;
+    margin-left: 8px;
+    width: 100%;
     padding: 0;
     background-color: var(--color-gray50);
   }
-  width:100%;
- 
+  width: 100%;
+  margin-left: 0px;
+
   .link {
     text-decoration: none;
   }
   .button {
-    @media (min-width: 700px){
+    @media (min-width: 700px) {
       width: 200px;
       position: sticky;
       text-align: center;
     }
-      width: 100%;
-      font-size: 14px;
-      margin-bottom: 20px;
+    width: 100%;
+    font-size: 14px;
+    margin-bottom: 20px;
   }
   div {
     height: 60px;
@@ -49,31 +51,31 @@ export const ButtonsContainer = styled.div`
   } */
 `;
 export const ListContainer = styled.ul`
- @media (min-width: 700px){
-  display: flex;
-  flex-wrap: wrap;
-  max-width: 774px;
-  max-height: 451px;
-  overflow-y: auto;
-  margin-top: 20px;
+  @media (min-width: 1100px) {
+    display: flex;
+    flex-wrap: wrap;
+    max-width: 774px;
+    max-height: 451px;
+    overflow-y: auto;
+    margin-top: 20px;
 
-  /* align-items: center;
+    /* align-items: center;
   justify-content:center; */
-  margin-left: 20px;
+    margin-left: 20px;
 
-  li {
-    list-style-type: none;
-    width: 50vw;
-    margin-top: 5px;
-    border-radius: 10px;
-    text-align: center;
-    /* height: 30%; */
-  }
-  width: 100%;
-  li{
+    li {
+      list-style-type: none;
+      width: 50vw;
+      margin-top: 5px;
+      border-radius: 10px;
+      text-align: center;
+      /* height: 30%; */
+    }
     width: 100%;
+    li {
+      width: 100%;
+    }
   }
-}
   /* @media (max-width: 600px) {
     flex-direction: column;
     align-items: center;
